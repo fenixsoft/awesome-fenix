84e8ae6c1683d1599744d02f07554bdb23842307
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-19 17:24:33 +0800

    Deploy fenixsoft/awesome-fenix to github.com/fenixsoft/awesome-fenix.git:gh-pages

--- assets/js/56.79b20418.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[56],{143:function(n,w,o){}}]);
---
+++ assets/js/58.41562a66.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[58],{143:function(n,w,o){}}]);
