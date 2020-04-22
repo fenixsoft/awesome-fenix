0bea991096cb51ecb4f7da50e261c8ddf31a51f0
icyfenix <icyfenix@gmail.com>
2020-04-22 12:20:07 +0800

    Merge branch 'master' of https://github.com/fenixsoft/awesome-fenix

--- .vuepress/components/PublishInfos.vue
+++ .vuepress/components/PublishInfos.vue
@@ -2,13 +2,11 @@
     <span class="only-for-printer" style="padding-top:300px">
         <br/>周志明
         <br/>icyfenix@gmail.com
-        <br/>发行日期：<words type='updatedText' />
+        <br/>发行日期：<Words type='updatedText' />
     </span>
 </template>
 
 <script>
-    import Words from './Words'
-
     export default {
         name: "PublishInfos"
     }
