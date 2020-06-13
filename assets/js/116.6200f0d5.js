23ab5d1fb43a282b89af93b7738a16ccaf20d85d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-13 13:17:15 +0800

    Deploy fenixsoft/awesome-fenix to github.com/fenixsoft/awesome-fenix.git:gh-pages

--- assets/js/116.6200f0d5.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[116],{550:function(t,s,e){"use strict";e.r(s);var n=e(11),i=Object(n.a)({},(function(){var t=this.$createElement,s=this._self._c||t;return s("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[s("h1",{attrs:{id:"服务网格：istio"}},[this._v("服务网格：Istio")])])}),[],!1,null,null,null);s.default=i.exports}}]);
---
+++ assets/js/124.6d4e3c1d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[124],{559:function(t,s,e){"use strict";e.r(s);var n=e(11),r=Object(n.a)({},(function(){var t=this.$createElement,s=this._self._c||t;return s("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[s("h1",{attrs:{id:"crd定义"}},[this._v("CRD定义")])])}),[],!1,null,null,null);s.default=r.exports}}]);
