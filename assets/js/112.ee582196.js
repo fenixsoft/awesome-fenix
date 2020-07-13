20aaa91f65c611d9b2ccf0df0f58f57f0d7300e0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-13 17:57:22 +0800

    Deploy fenixsoft/awesome-fenix to github.com/fenixsoft/awesome-fenix.git:gh-pages

--- assets/js/112.ee582196.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[112],{553:function(t,e,s){"use strict";s.r(e);var r=s(11),n=Object(r.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("h1",{attrs:{id:"无服务：serverless"}},[this._v("无服务：Serverless")])])}),[],!1,null,null,null);e.default=n.exports}}]);
