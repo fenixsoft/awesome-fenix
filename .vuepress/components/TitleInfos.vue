af1eda18b6fbc1be6c71a52c69462a232b8d0515
icyfenix <icyfenix@gmail.com>
2021-06-01 10:48:32 +0800

    Merge branch 'master' of https://github.com/fenixsoft/awesome-fenix

--- .vuepress/components/TitleInfos.vue
+++ .vuepress/components/TitleInfos.vue
@@ -2,8 +2,7 @@
     <span style="text-align: center;padding: 0 0 20px 0;display: block;">
         <h1 style="font-size: 2.6rem;margin: 10px 0 10px 0;font-family: cursive;">凤凰架构</h1>
     ​    <span style="font-size: 20px;font-weight: 400; font-family: cursive;">
-            构筑可靠的大型分布式系统
-            <Badge text=" Beta " type="warning"/>
+            构建可靠的大型分布式系统
         </span>
     </span>
 </template>
